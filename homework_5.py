9a1f17c9afc1b6ee415c940a9a196e95d397ff19
Anton P <a.s.panferov@mail.ru>
2022-12-09 18:48:53 +0300

    сделал дз 5

--- homework_5.py
+++ homework_5.py
@@ -1,3 +1,31 @@
+print ('hello world')
+# Программа записывает в файл слова, читает их, 
+# Ищет среди них слова "о" и "л" и записывает в новый файл
+words = open('words.txt', 'w', encoding='utf-8')
+for _ in range(6):
+    words.writelines(input('Введите слово: '))
+    words.writelines('\n')
+words.close()
+
+some_list = list(open('words.txt', 'r', encoding='utf-8'))
+# print(some_list)
+out_list = []
+for i in range (0, len(some_list)):
+    if 'о' and 'л' not in some_list[i]:
+        replace = some_list[i]
+        some_list[i] = replace[:-1]
+        out_list.append(some_list[i])      
+# print(out_list)
+
+data = open('res_words.txt', 'w', encoding='utf-8')
+for i in out_list:
+    data.writelines(i)
+    data.writelines('\n')
+data.close()
+
+# Игра в крестики-нолики на двух игроков, игровое поле - 3х3.
+
+
 print('Перед вами игровое поле')
 gameBoard = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 print(f'| {gameBoard[0]} || {gameBoard[1]} || {gameBoard[2]} |', 
@@ -33,7 +61,8 @@ def hod (pobeda, player, game_step):
     return step_pobeda
 
 
-while 'ooo' or 'xxx' not in pobeda:
+while 'ooo' not in pobeda and 'xxx' not in pobeda:
+    print(f'Ход {count}')
     game_step = input(f'Игрок {playing} ходит: ')
     if playing == 1:
         pobeda = hod (pobeda, player_1, game_step)
@@ -45,6 +74,10 @@ while 'ooo' or 'xxx' not in pobeda:
         playing = 1
     count += 1
     print(pobeda)
-    print(f'Ход {count}')
-    
-print(f'Игрок {playing} победил!!!')
+
+if playing == 1: 
+    playing = 2
+    print(f'Игрок {playing} победил!!!')
+elif playing == 2: 
+    playing = 1
+    print(f'Игрок {playing} победил!!!')
